89d4e618d986b8c5d20acf0f2837700ef7f205b1
Andrii Khoma <anik123wb@gmail.com>
2026-07-27 05:28:56 +0300

    cmd

---
+++ JBD.cmd
@@ -0,0 +1,6 @@
+python editor/editor.py ^
+  --font fonts/Caveat-Fixed_centerline/Caveat-Fixed_centerline.svg ^
+  --orientation portrait ^
+  --margin-left 75 --margin-top 30 --margin-right 25 --margin-bottom 4 ^
+  --size 3.10 --line-height 5.05 --line-gap 0 ^
+  --label дата:-32:31
---
+++ JOVR.cmd
@@ -0,0 +1,6 @@
+python editor/editor.py ^
+  --font fonts/Caveat-Fixed_centerline/Caveat-Fixed_centerline.svg ^
+  --orientation landscape ^
+  --margin-left 57 --margin-right 99 --margin-top 44 --margin-bottom 10 ^
+  --size 3.10 --line-height 3.366666 --line-gap 0 ^
+  --label номер:-55:8 --label дата:-47:12 --label кому:-23:20
